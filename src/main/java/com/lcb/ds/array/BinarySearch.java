7cfed8651d380824b6037081ee9c6cc57b575b9e
zhaoxl <zhxluke@163.com>
2019-12-11 09:12:43 +0800

    binary search

--- src/main/java/com/lcb/ds/array/BinarySearch.java
+++ src/main/java/com/lcb/ds/array/BinarySearch.java
@@ -9,9 +9,9 @@ public class BinarySearch {
     public static void main(String[] args) {
         int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15 };
 
-        System.out.println("" + f(16, array));
+        System.out.println("" + f(5, array));
 
-        System.out.println("" + f(16, array, 0, array.length - 1));
+        System.out.println("" + f(5, array, 0, array.length - 1));
     }
 
     public static int f(int a, int[] array) {
